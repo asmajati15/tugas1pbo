e2cfbfd89b3a587c70d0fe1a7f4ca0b6ac5f0dfe
AAProject <87751322+asmajati15@users.noreply.github.com>
2021-08-20 14:35:02 +0700

    Add files via upload

---
+++ TugasPBO1.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugaspbo1;
+
+/**
+ *
+ * @author Asmajati_15
+ */
+public class TugasPBO1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //System.out.println("Hello World");
+        siswa siswa1=new siswa();
+        siswa siswa2=new siswa();
+        System.out.println("Nama saya " + siswa1.nama);
+        System.out.println(siswa1.umur=19);
+        System.out.println(siswa1.tinggi=175);
+        
+        System.out.println(siswa2.nama="Samid");
+        System.out.println(siswa2.umur=15);
+        System.out.println(siswa2.tinggi=170);
+    }
+    
+}
---
+++ siswa.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugaspbo1;
+
+/**
+ *
+ * @author Asmajati_15
+ */
+public class siswa {
+    String nama="Affad";
+    Integer umur;
+    Integer tinggi;    
+}
